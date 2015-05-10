91c01ce3480c1c0c368dda241e7568a9b0fbadd9
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-05-10 09:46:52 +0200

    hotkey display

--- conf.lua
+++ conf.lua
@@ -1,7 +1,15 @@
+--- @module conf
+local conf = {}
 
+conf.SCREEN_WIDTH = 400
+conf.SCREEN_HEIGHT = 300
+conf.SCALE_GRAPHICS = 1.0 
 
 -------------------------------------------------------------------------------
 function love.conf(t)
-    t.window.width = 800 -- t.screen.width in 0.8.0 and earlier
-    t.window.height = 600 -- t.screen.height in 0.8.0 and earlier
+    t.window.width = conf.SCREEN_WIDTH -- t.screen.width in 0.8.0 and earlier
+    t.window.height = conf.SCREEN_HEIGHT -- t.screen.height in 0.8.0 and earlier
 end
+
+-------------------------------------------------------------------------------
+return conf
--- main.lua
+++ main.lua
@@ -2,17 +2,30 @@
 local Font = nil
 
 local rgb = require "rgb"
+local conf = require "conf"
+local imgui = require "imgui"
+local gui = require "gui"
 
-local OFFSET_X = 100
-local OFFSET_Y = 100
-local BASELINE = 0
+local OFFSET_X = 50
+local OFFSET_Y = 50
+
+-------------------------------------------------------------------------------
+local function rescale()
+	love.window.setMode( 
+		conf.SCREEN_WIDTH * conf.SCALE_GRAPHICS, 
+		conf.SCREEN_HEIGHT * conf.SCALE_GRAPHICS, 
+		{ vsync = true, resizable = false } 
+	)	
+	love.window.setTitle( string.format( "%d x %d, scale=%.2f", conf.SCREEN_WIDTH, conf.SCREEN_HEIGHT, conf.SCALE_GRAPHICS ) )
+end
 
 -------------------------------------------------------------------------------
 function love.load()
-	--Font = love.graphics.newFont( "OpenSans-Regular.ttf", 100 )
-	Font = love.graphics.newFont( "Crimson-Bold.ttf", 100 )
+	--Font = love.graphics.newFont( "OpenSans-Regular.ttf", 50 )
+	Font = love.graphics.newFont( "Crimson-Bold.ttf", 50 )
+	Font:setLineHeight( 0.6 )
 	love.graphics.setFont( Font )
-	BASELINE = OFFSET_Y + Font:getBaseline()
+	rescale()
 end
 
 -------------------------------------------------------------------------------
@@ -26,26 +39,78 @@ local function setColor( name )
 end
 
 -------------------------------------------------------------------------------
-function drawLine( y, color )
+function drawLineH( y, color )
     setColor( color )
-    love.graphics.line( 0, BASELINE - y, 800, BASELINE - y )	
+    love.graphics.line( 0, y, conf.SCREEN_WIDTH, y )	
+end
+
+-------------------------------------------------------------------------------
+function drawLineV( x, color )
+    setColor( color )
+    love.graphics.line( x, 0, x, conf.SCREEN_HEIGHT  )	
+end
+
+
+-------------------------------------------------------------------------------
+function hotkeyOffset( font, text, char )
+	local a, b = string.find( text, char )
+	local pretext = string.sub( text, 1, a )
+	return Font:getWidth( pretext ) - Font:getWidth( char )
+end
+
+-------------------------------------------------------------------------------
+function alignOffset( box_width, text_width, align )
+	if align == "left" then 
+		return 0
+	elseif align == "right" then
+		return box_width - text_width
+	elseif align == "center" then
+		return ( box_width - text_width ) / 2
+	else
+		error( "unsupported align mode:" .. align )
+	end
 end
 
 -------------------------------------------------------------------------------
 function love.draw()
-	setColor "white"
+	love.graphics.scale( conf.SCALE_GRAPHICS )
+	love.graphics.translate( OFFSET_X, OFFSET_Y )
 
-    love.graphics.print( "Hello ypqjg", OFFSET_X, OFFSET_Y )
+	setColor "brown"
+	love.graphics.polygon('fill', 0, 0, 200, 100, 150, 200)
+
+	local text = "Hello iwiwiw"
+	local box_width = 350
+	local text_width = Font:getWidth( text )
+	drawLineV( box_width, "white" )
+	local align = "center"
+    love.graphics.printf( text, 0, 0, box_width, align )
+    setColor "gold"
+    love.graphics.print( "w", hotkeyOffset(Font, text, "w") + alignOffset( box_width, text_width, align ), 0 )
     
-    drawLine( Font:getAscent(), "red" )
-    drawLine( 0, "green" )
-    drawLine( Font:getDescent(), "blue" )
-    drawLine( Font:getHeight(), "yellow" )
+    drawLineH( Font:getBaseline(), "green" )
+    drawLineH( Font:getHeight() - Font:getBaseline(), "red" )
+    drawLineH( Font:getHeight(), "yellow" )
+    drawLineV( Font:getWidth( text ), "yellow" )
 end
 
 -------------------------------------------------------------------------------
 function love.keypressed( key )
 	if key == "escape" then
 		love.event.quit()
+	elseif key == "up" or key == "w" then
+		if conf.SCALE_GRAPHICS < 1 then
+			conf.SCALE_GRAPHICS = conf.SCALE_GRAPHICS * 2
+		else
+			conf.SCALE_GRAPHICS = conf.SCALE_GRAPHICS + 1
+		end
+		rescale()
+	elseif key == "down" or key == "s" then
+		if conf.SCALE_GRAPHICS > 1 then
+			conf.SCALE_GRAPHICS = conf.SCALE_GRAPHICS - 1
+		else
+			conf.SCALE_GRAPHICS = conf.SCALE_GRAPHICS / 2
+		end
+		rescale()
 	end
 end
